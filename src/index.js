705898f21528f692b976b8b88a05d337a5bdf0d6
jakebroffman <jake.broffman1088@gmail.com>
2023-01-24 16:49:14 -0500

    commit tree d8dfb4ba447322f2e434053fa1a1c5676627cd84

--- src/index.js
+++ src/index.js
@@ -12,4 +12,78 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  function fetchData(url) {
+    return fetch(url)
+    .then(res => res.json())
+  }
+
+  function postData(url, body) {
+    return fetch(url,{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: "application/json"
+      },
+      body: JSON.stringify(body)
+    })
+    .then(res => res.json())
+  }
+
+  function handleForm(e) {
+    e.preventDefault()
+
+    const toy = {
+      name: e.target.name.value,
+      image: e.target.image.value,
+      likes: 0
+    }
+    postData('http://localhost:3000/toys', toy)
+    .then(data => renderToyCard(data)) 
+    .catch(e => console.error(e))
+  }
+ 
+  function renderToyCard(toyData) {
+    const div = document.createElement('div')
+    const h2 = document.createElement('h2')
+    const img = document.createElement('img')
+    const p = document.createElement('p')
+    const btn = document.createElement('button')
+  
+    h2.textContent = toyData.name
+    p.textContent = `${toyData.likes} Likes!`
+    btn.textContent = 'Like <3'
+  
+    img.src = toyData.image
+    img.className = 'toy-avatar'
+    div.className = 'card'
+    btn.className = 'like-btn'
+    btn.id = toyData.id
+
+    btn.addEventListener('click', () => {
+      fetch(`http://localhost:3000/toys/${toyData.id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        },
+        body: JSON.stringify({likes:toyData.likes+1})
+      })
+      .then(res => res.json())
+      .then(data => renderToyCard(data))
+    })
+  
+    div.append(h2,img,p,btn)
+    document.querySelector('#toy-collection').append(div)
+  }
+  
+  fetchData('http://localhost:3000/toys')
+  .then(data => data.forEach(renderToyCard))
+  .catch(error => {
+    console.error(error)
+    document.querySelector('main').innerHTML = `<h2>Sorry, we will be back soon!</h2>`
+  })
+
+  document.querySelector('form').addEventListener('submit', handleForm);
 });
+
